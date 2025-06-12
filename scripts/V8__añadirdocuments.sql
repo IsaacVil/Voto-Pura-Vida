97a7698042b4c29c7969aad8b717a31ea1da191c
Isaac Villalobos <isaacvillalobosb@gmail.com>
2025-06-12 16:42:48 -0600

    errores

--- scripts/V8__añadirdocuments.sql
+++ scripts/V8__añadirdocuments.sql
@@ -1,9 +1,6 @@
 use [VotoPuraVida]
 go
-DELETE FROM PV_AIDocumentAnalysis;
-DELETE FROM PV_proposalCommentDocuments;
-DELETE FROM PV_proposalComments;
-DELETE FROM PV_Documents;
+
 insert into PV_workflowstype(name) values ('Revision de documentos');
 insert into PV_workflows(name, description, endpoint, workflowTypeId, params)
 values (
